056767fb286a3253b80d72c7018c1f8c5ffb1fc7
onkesh-basis <147563189+onkesh-basis@users.noreply.github.com>
2024-01-24 00:45:30 +0000

    added changes

--- script.js
+++ script.js
@@ -1,24 +1,21 @@
 // 1. Convert to an arrow function.
-function greet() {
+const greet = () => {
     return "Hello, world!";
-}
+};
   
 
 // 2. Convert to an arrow function.
-function double(number) {
+const double = (number) => {
     return number * 2;
 }
   
 
 // 3. Convert to an arrow function.
-function multiply(a, b) {
+const multiply = (a, b) => {
     return a * b;
 }
-  
-  
 
 console.log(greet())  // should print "Hello, world!"
 console.log(double(3)) // should print 6
 console.log(multiply(3, 7)) // should print 21
-  
   
